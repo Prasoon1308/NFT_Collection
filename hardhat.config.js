4d77199edbcc4b4b1e445c23c6bb987e27882f85
Prasoon <prasoon1308@gmail.com>
2023-04-05 00:56:07 +0530

    hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    // mainnet: {
+    //   url: process.env.PROD_QUICKNODE_KEY,
+    //   accounts: [process.env.PRIVATE_KEY],
+    // },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
